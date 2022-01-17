1b1ebd2df277fe4bdcfdf0279db6b95e1027c00b
joaociardullo <joaociardullo@hotmail.com>
2022-01-17 15:56:46 -0300

    Classe user service impl FindById

---
+++ src/main/java/br/com/devjoao/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package br.com.devjoao.api.services.impl;
+
+import br.com.devjoao.api.domain.User;
+import br.com.devjoao.api.repositories.UserRepository;
+import br.com.devjoao.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+    @Override
+    public User FindById(Integer id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
