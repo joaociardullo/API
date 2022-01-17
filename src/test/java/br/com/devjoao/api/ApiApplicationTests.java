618490ecc514296c7b267fc5799bce484b61070e
joaociardullo <joaociardullo@hotmail.com>
2022-01-17 14:51:05 -0300

    Projeto Iniciado

---
+++ src/test/java/br/com/devjoao/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devjoao.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
