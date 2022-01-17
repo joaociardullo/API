0979bbfd97399eff4e93572f04c3e011768a6758
joaociardullo <joaociardullo@hotmail.com>
2022-01-17 16:23:04 -0300

    tratamento de execeção para objeto nao encotrado

--- src/main/java/br/com/devjoao/api/ApiApplication.java
+++ src/main/java/br/com/devjoao/api/ApiApplication.java
@@ -1,4 +1,4 @@
-package br.com.devjoao.api;
+package br.com.dicasdeumdev.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/br/com/devjoao/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/devjoao/api/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.devjoao.api.services.impl;
 import br.com.devjoao.api.domain.User;
 import br.com.devjoao.api.repositories.UserRepository;
 import br.com.devjoao.api.services.UserService;
+import br.com.devjoao.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User FindById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
