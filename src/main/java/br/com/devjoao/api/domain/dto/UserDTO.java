0a62d0418410296df5c72346abe1c2ca74b8dc3f
joaociardullo <joaociardullo@hotmail.com>
2022-01-17 16:27:49 -0300

    Criação de User DTO

---
+++ src/main/java/br/com/devjoao/api/domain/dto/UserDTO.java
@@ -0,0 +1,19 @@
+package br.com.devjoao.api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
